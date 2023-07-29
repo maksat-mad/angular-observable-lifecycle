987be6c8e25702f7850bae3867cdec19e4c49eca
1234 <a1234@1234s-MacBook-Pro.local>
2023-07-29 18:33:38 +0600

    angular observable lifecycle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'observable-lifecycle'`, () => {
+  it(`should have as title 'angular-observable-lifecycle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('observable-lifecycle');
+    expect(app.title).toEqual('angular-observable-lifecycle');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('observable-lifecycle app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-observable-lifecycle app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'observable-lifecycle';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'angular-observable-lifecycle';
+  subscription$: Subscription | undefined;
+
+  ngOnInit() {
+    let observable$ = new Observable((observer: any) => {
+      // case 1
+      // observer.next('A');
+      // observer.next('B');
+      // observer.error('Error occured.');
+      // observer.complete('Data stream is over.');
+      // observer.next('C');
+      // output: A B
+
+      // case 2
+      // observer.next('A');
+      // observer.next('B');
+      // observer.complete('Data stream is over.');
+      // observer.next('C');
+      // output: A B
+
+      // case 3
+      // observer.next('A');
+      // observer.next('B');
+      // observer.complete('Data stream is over.');
+      // observer.next('C');
+      // observer.error('Error occured.');
+      // output: A B
+
+      // case 3
+      // observer.next('A');
+      // observer.next('B');
+      // observer.complete('Data stream is over.');
+      // observer.error('Error occured.');
+      // observer.next('C');
+      // output: A B
+
+      // case 4
+      observer.next('A');
+      observer.next('B');
+      observer.next('A');
+      observer.next('B');
+      setInterval(() => {
+        observer.next("This is a random message.");
+      }, 2000);
+      observer.next('C');
+        observer.next('D');
+      setInterval(() => {
+        observer.error("Error occured.");
+      }, 4001);
+      observer.next('E');
+    });
+    // output: A B A B C D E random2 error
+
+    this.subscription$ = observable$.subscribe(function showMessage(msg: any) {
+      console.log(msg);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription$?.unsubscribe();
+  }
 }
